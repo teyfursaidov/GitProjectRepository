f91a81f6157ba8ac1d033dd8fec856ae3938b3e2
teyfursaidov <teyfursaidov1991@gmail.com>
2022-05-14 19:02:59 +0300

    2nd Commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("GIT Project Test Code");
+        System.out.println("GIT Project Test Code.");
+        System.out.println("Updated Test Code 1.");
+        System.out.println("Updated Test Code 2.");
+        System.out.println("Updated Test Code 3.");
 
 
 
